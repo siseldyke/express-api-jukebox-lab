e16011a8b09b07c5cd2e69cea7631ea48a218c5a
siseldyke <siseldyke@gmail.com>
2024-11-07 16:07:29 -0500

    all crud working

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -42,43 +42,43 @@ router.get('/:trackId', async (req, res) => {
   });
 
   
-// router.delete('/:trackId', async (req, res) => {
-//     try { const foundTrack = await Track.findByIdAndDelete(req.params.petId)
+router.delete('/:trackId', async (req, res) => {
+    try { const foundTrack = await Track.findByIdAndDelete(req.params.trackId)
         
-//         if (!foundTrack) {
-//             res.status(404)
-//             throw new Error('This is a test error');
-//         }
-//         res.status(200).json(`${foundTrack.title} has been deleted.`);
-//         } catch (error) {
-//             if (res.statusCode === 404) {
-//                 res.json({ error: error.message });
-//               } else {
-//              res.status(500).json({ error: error.message });
-//         }
-//     }
-//   });
+        if (!foundTrack) {
+            res.status(404)
+            throw new Error('This is a test error');
+        }
+        res.status(200).json(`${foundTrack.title} has been deleted.`);
+        } catch (error) {
+            if (res.statusCode === 404) {
+                res.json({ error: error.message });
+              } else {
+             res.status(500).json({ error: error.message });
+        }
+    }
+  });
 
 
 
 
-// router.put('/:trackId', async (req, res) => {
-//     try {
-//       const updatedTrack = await Track.findByIdAndUpdate(req.params.trackId, req.body,  {new: true,});
-//       if (!updatedTrack) {
-//         res.status(404);
-//         throw new Error('Pet not found.');
-//       }
-//       res.status(200).json(updatedTrack);
-//     } catch (error) {
-//       // Add code for errors
-//       if (res.statusCode === 404) {
-//         res.json({ error: error.message });
-//       } else {
-//         res.status(500).json({ error: error.message });
-//       }
-//     }
-//   });
+router.put('/:trackId', async (req, res) => {
+    try {
+      const updatedTrack = await Track.findByIdAndUpdate(req.params.trackId, req.body,  {new: true,});
+      if (!updatedTrack) {
+        res.status(404);
+        throw new Error('Pet not found.');
+      }
+      res.status(200).json(updatedTrack);
+    } catch (error) {
+      // Add code for errors
+      if (res.statusCode === 404) {
+        res.json({ error: error.message });
+      } else {
+        res.status(500).json({ error: error.message });
+      }
+    }
+  });
   
 
 module.exports = router;
